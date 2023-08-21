9c0ff0acca14038353807888425574d7cb523711
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-21 11:31:21 +0300

    completed assignment

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,12 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+        
+    def walk(Self):
+        print("The person is walking.")    
+
+john = Person()
+john.talk()
+john.walk()
